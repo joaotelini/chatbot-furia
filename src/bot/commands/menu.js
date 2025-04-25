d9e012bb9e3ce1576e07a3822b4a605c85303776
João Pedro Telini <joaotelini03@gmail.com>
2025-04-25 13:31:15 -0300

    melhora na mensagem do /menu

--- src/bot/commands/menu.js
+++ src/bot/commands/menu.js
@@ -1,10 +1,36 @@
 export function menuCommand(bot) {
   bot.onText(/\/menu/, async (msg) => {
     const chatId = msg.chat.id;
-    await bot.sendMessage(chatId, "Aqui estão os comandos disponíveis:");
-    await bot.sendMessage(
-      chatId,
-      "/start - Iniciar o bot\n/menu - Ver os comandos disponíveis\n/jogos - Ver os resultado de próximos jogos da Pantera\n/noticias - Ver as ultimas noticías\n/historia - Ver e conhecer de perto a história da organização\n/ranking - Ver a posição da FURIA no ranking da HLTV\n/ajuda - Ver ajuda sobre o bot\n/contato - Falar com o desenvolvedor"
-    );
+
+    const menuMessage = `
+  🔥 *Bem-vindo ao menu da Pantera!* 🔥Escolha uma das opções abaixo para explorar tudo sobre a FURIA Esports:
+  `;
+
+    const keyboard = {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: "📅 Próximos Jogos", callback_data: "jogos" },
+            { text: "📊 Resultados Recentes", callback_data: "resultados" },
+          ],
+          [
+            { text: "👥 Lineup Atual", callback_data: "roster" },
+            { text: "🎯 Jogadores", callback_data: "players" },
+          ],
+          [
+            { text: "📰 Últimas Notícias", callback_data: "noticias" },
+            { text: "📖 História da FURIA", callback_data: "historia" },
+          ],
+          [{ text: "🏆 Ranking HLTV", callback_data: "ranking" }],
+          [
+            { text: "❓ Ajuda", callback_data: "ajuda" },
+            { text: "📨 Contato", callback_data: "contato" },
+          ],
+        ],
+      },
+      parse_mode: "Markdown",
+    };
+
+    await bot.sendMessage(chatId, menuMessage, keyboard);
   });
 }
