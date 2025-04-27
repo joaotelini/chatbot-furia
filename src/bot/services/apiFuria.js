1766d4244f1fc5ba47dc0e337d86dfd0afcb8f3b
João Pedro Telini <joaotelini03@gmail.com>
2025-04-27 01:35:10 -0300

    adiciona axios como dependência e implementa funções para obter dados de jogador, elenco, próximo jogo e resultados

---
+++ src/bot/services/apiFuria.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const API_URL = process.env.API_URL;
+
+export async function getPlayer(player) {
+  try {
+    const response = await axios.get(`${API_URL}/api/player/${player}`);
+    const playerData = response.data;
+    console.log(playerData);
+    return playerData;
+  } catch (error) {
+    console.error("Error fetching player data:", error);
+    return null;
+  }
+}
+
+export async function getRoster() {
+  try {
+    const response = await axios.get(`${API_URL}/api/roster`);
+    const rosterData = response.data;
+    console.log(rosterData);
+    return rosterData;
+  } catch (error) {
+    console.error("Error fetching roster data:", error);
+    return null;
+  }
+}
+
+export async function getNextMatch() {
+  try {
+    const response = await axios.get(`${API_URL}/api/nextmatch`);
+    const matchData = response.data;
+    console.log(matchData);
+    return matchData;
+  } catch (error) {
+    console.error("Error fetching next match data:", error);
+    return null;
+  }
+}
+
+export async function getResults() {
+  try {
+    const response = await axios.get(`${API_URL}/api/results`);
+    const resultsData = response.data;
+    console.log(resultsData);
+    return resultsData;
+  } catch (error) {
+    console.error("Error fetching results data:", error);
+    return null;
+  }
+}
